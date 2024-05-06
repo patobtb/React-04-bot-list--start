d5dc9322107a3ba85a65db0ae83b1bd7a36b22a6
Pato Brugmann <patobrugmann@gmail.com>
2024-05-06 05:38:01 +0300

    created the list component and complited task three

--- src/view/list.jsx
+++ src/view/list.jsx
@@ -1,7 +1,14 @@
-export function List({}) {
+import { Card } from "./card";
+
+export function List({botsList}) {
   return (
     <div className="cards-list">
-      <ul>{"???"}</ul>
+      <ul>
+        {botsList.map((item) => (
+        <li className="card-item" key={item.id}>
+          <Card {...item} />
+        </li>
+      ))}</ul>
     </div>
   );
 }
